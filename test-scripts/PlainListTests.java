a369dc8f5d22b6de659723c642b74560f3570467
Tilman Holube <tilman@holube.de>
2024-03-20 13:17:05 +0100

    testing jdk 22

--- test-scripts/PlainListTests.java
+++ test-scripts/PlainListTests.java
@@ -8,64 +8,60 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class PlainListTests {
+boolean fail = false;
 
-    private static boolean fail = false;
+void main(String[] args) throws IOException {
+    Files.walkFileTree(Path.of("./plain"), new SimpleFileVisitor<>() {
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            List<String> lines = Files.readAllLines(file).stream().toList();
 
-    public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./plain"), new SimpleFileVisitor<>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                List<String> lines = Files.readAllLines(file).stream().toList();
-
-                // test if sorted
-                final List<String> sortedLines = lines.stream().sorted().toList();
-                if (!lines.equals(sortedLines)) {
-                    fail = true;
-                    print("%s is not sorted%n", file);
-                }
-                lines = sortedLines;
-
-                final List<String> newLines = new ArrayList<>(sortedLines.size());
-                String previousLine = "";
-                for (int i = 0; i < lines.size(); i++) {
-                    final String line = lines.get(i);
-                    if (line.isBlank()) {
-                        fail = true;
-                        print("%s empty line%n", file);
-                    } else if (line.contains(" ") || line.contains("\t")) {
-                        fail = true;
-                        String l = line.trim();
-                        if (line.contains(" ") || line.contains("\t")) {
-                            newLines.add(l);
-                        }
-                        print("%s:%s line with whitespace%n", file, i);
-                    } else if (previousLine.equals(line)) { // remove duplicates
-                        fail = true;
-                        print("%s duplicate word: %s%n", file, line);
-                    } else {
-                        newLines.add(line);
-                    }
-                    previousLine = line;
-                }
-
-                Optional<String> fileString = newLines.stream().sorted().reduce((a, b) -> a + "\n" + b);
-                if (fileString.isPresent())
-                    Files.writeString(file, fileString.get());
-                else {
-                    fail = true;
-                    print("could not create string to write to file");
-                }
-
-                return FileVisitResult.CONTINUE;
+            // test if sorted
+            final List<String> sortedLines = lines.stream().sorted().toList();
+            if (!lines.equals(sortedLines)) {
+                fail = true;
+                print("%s is not sorted%n", file);
             }
-        });
-        if (fail)
-            System.exit(1);
-    }
+            lines = sortedLines;
 
-    private static void print(String format, Object... objects) {
-        System.err.printf(format, objects);
-    }
+            final List<String> newLines = new ArrayList<>(sortedLines.size());
+            String previousLine = "";
+            for (int i = 0; i < lines.size(); i++) {
+                final String line = lines.get(i);
+                if (line.isBlank()) {
+                    fail = true;
+                    print("%s empty line%n", file);
+                } else if (line.contains(" ") || line.contains("\t")) {
+                    fail = true;
+                    String l = line.trim();
+                    if (line.contains(" ") || line.contains("\t")) {
+                        newLines.add(l);
+                    }
+                    print("%s:%s line with whitespace%n", file, i);
+                } else if (previousLine.equals(line)) { // remove duplicates
+                    fail = true;
+                    print("%s duplicate word: %s%n", file, line);
+                } else {
+                    newLines.add(line);
+                }
+                previousLine = line;
+            }
 
+            Optional<String> fileString = newLines.stream().sorted().reduce((a, b) -> a + "\n" + b);
+            if (fileString.isPresent())
+                Files.writeString(file, fileString.get());
+            else {
+                fail = true;
+                print("could not create string to write to file");
+            }
+
+            return FileVisitResult.CONTINUE;
+        }
+    });
+    if (fail)
+        System.exit(1);
+}
+
+void print(String format, Object... objects) {
+    System.err.printf(format, objects);
 }
